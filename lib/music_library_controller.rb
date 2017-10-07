d59b8e3754af03188a22fdc6a547bcab3789510d
alpbourne <alpbourne@gmail.com>
2017-10-07 03:38:29 +0000

    automatically backed up by learn

--- lib/music_library_controller.rb
+++ lib/music_library_controller.rb
@@ -51,7 +51,7 @@ class MusicLibraryController
   def list_genres
     i = 0
     genre_list = Genre.all.sort_by {|genre| genre.name}
-    genre.list.each {|genre| puts "#{i+=1}. #{genre.name}"}
+    genre_list.each {|genre| puts "#{i+=1}. #{genre.name}"}
   end
 
 end
